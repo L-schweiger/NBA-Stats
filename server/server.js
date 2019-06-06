a327adb720c9cb82cf182f4f2d9eef4372493aa2
L-schweiger <l.schweiger@studierende.htl-donaustadt.at>
2019-06-06 18:19:48 +0200

    +bootstrap eingebunden

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var server = express();
 var path = require('path');
+//var bootstrap = require('bootstrap'); umgangen mit direkter css und js-einbindung per pfad
+
+server.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+server.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+server.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 server.get('/', function(request, reponse) {
     reponse.sendFile('index.html', {root: path.join(__dirname, './html')});
